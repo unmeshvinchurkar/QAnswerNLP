618c7445eee3a20e67cefca0fa2ad97732142704
Unmesh Vinchurkar <unmeshvinchurkar@fico.com>
2017-10-03 00:08:02 +0530

    Added extractor.

---
+++ src/com/nlp/DependencyExtractor.java
@@ -0,0 +1,175 @@
+package com.nlp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class DependencyExtractor {
+
+	public static String SUBJECT = "nsubj";
+	public static String OBJECT = "dobj";
+	public static String IN = "prep_in";
+	public static String OF = "prep_of";
+	public static String VERB_VERB_PHRASE = "xcomp";
+	public static String TIME_MOD = "tmod";
+	public static String ADJ_NOUN = "amod";
+	public static String VB_IN_NOUN = "nmod"; // recorded in India
+	public static String COMPOUND_WORDS = "compound";
+
+	private Map<String, Set<String>> verb_Subject = new HashMap<>();
+	private Map<String, Set<String>> verb_Object = new HashMap<>();
+
+	private Map<String, Set<String>> verb_in_noun_map = new HashMap<>();
+	private Map<String, Set<String>> adj_noun_map = new HashMap<>();
+	private Map<String, Set<String>> verb_verb_map = new HashMap<>();
+
+	private Map<String, Set<String>> compoundWords = new HashMap<>();
+
+	public DependencyExtractor() {
+
+	}
+
+	public void extractDependencies(String collapsedDeps) throws IOException {
+
+		BufferedReader bufReader = new BufferedReader(new StringReader(collapsedDeps));
+
+		String line = null;
+		while ((line = bufReader.readLine()) != null) {
+
+			if (line.startsWith(SUBJECT)) {
+				String verb_noun[] = getPair(line);
+
+				Set<String> set = verb_Subject.get(verb_noun[0]);
+
+				if (set == null) {
+					set = new HashSet<>();
+					verb_Subject.put(verb_noun[0], set);
+				}
+
+				set.add(verb_noun[1]);
+
+			} else if (line.startsWith(OBJECT)) {
+
+				String verb_noun[] = getPair(line);
+
+				Set<String> set = verb_Object.get(verb_noun[0]);
+				if (set == null) {
+					set = new HashSet<>();
+					verb_Object.put(verb_noun[0], set);
+				}
+				set.add(verb_noun[1]);
+
+			} else if (line.startsWith(VB_IN_NOUN) || line.startsWith(IN)) {
+				String verb_noun[] = getPair(line);
+
+				Set<String> set = verb_in_noun_map.get(verb_noun[0]);
+				if (set == null) {
+					set = new HashSet<>();
+					verb_in_noun_map.put(verb_noun[0], set);
+				}
+				set.add(verb_noun[1]);
+
+			} else if (line.startsWith(ADJ_NOUN)) {
+				String adj_noun[] = getPair(line);
+
+				Set<String> set = adj_noun_map.get(adj_noun[0]);
+				if (set == null) {
+					set = new HashSet<>();
+					adj_noun_map.put(adj_noun[0], set);
+				}
+				set.add(adj_noun[1]);
+			}
+
+			else if (line.startsWith(VERB_VERB_PHRASE)) {
+
+				String verb_verb[] = getPair(line);
+
+				Set<String> set = verb_verb_map.get(verb_verb[0]);
+				if (set == null) {
+					set = new HashSet<>();
+					verb_verb_map.put(verb_verb[0], set);
+				}
+				set.add(verb_verb[1]);
+
+			} else if (line.startsWith(COMPOUND_WORDS)) {
+				String word_word[] = getPair(line);
+
+				if (getIndex(word_word[0]) < getIndex(word_word[1])) {
+
+					Set<String> set = compoundWords.get(word_word[0]);
+					if (set == null) {
+						set = new HashSet<>();
+						compoundWords.put(word_word[0], set);
+					}
+					set.add(word_word[0] + " " + word_word[1]);
+				} else {
+
+					Set<String> set = compoundWords.get(word_word[1]);
+					if (set == null) {
+						set = new HashSet<>();
+						compoundWords.put(word_word[1], set);
+					}
+					set.add(word_word[1] + " " + word_word[0]);
+				}
+			}
+		}
+
+		bufReader.close();
+	}
+
+	public void generateConcepts() {
+
+	}
+
+	public boolean areCompoundWords(String w1, String w2) {
+
+		if (compoundWords.containsKey(w1)) {
+			Set<String> set = compoundWords.get(w1);
+			for (String w : set) {
+				if (w.contains(w2)) {
+					return true;
+				}
+			}
+		}
+
+		else if (compoundWords.containsKey(w2)) {
+			Set<String> set = compoundWords.get(w2);
+			for (String w : set) {
+				if (w.contains(w1)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	public static int getIndex(String word) {
+		int i = word.indexOf("-");
+		return Integer.valueOf(word.substring(i + 1));
+	}
+
+	public static String getWord(String word) {
+		int i = word.indexOf("-");
+		return word.substring(0, i);
+	}
+
+	private String[] getPair(String dependency) {
+		int i1 = dependency.indexOf("(");
+		int i2 = dependency.indexOf(")");
+
+		String pair[] = dependency.substring(i1 + 1, i2).split(",");
+
+		if (pair != null) {
+			int i = 0;
+			for (String s : pair) {
+				pair[i++] = s.trim();
+			}
+		}
+		return pair;
+	}
+
+}
---
+++ src/com/useless/CoreNlpParser.java
@@ -0,0 +1,311 @@
+package com.useless;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
+
+
+
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.IndexedWord;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.semgraph.SemanticGraph;
+import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.trees.TreeCoreAnnotations.TreeAnnotation;
+import edu.stanford.nlp.util.CoreMap;
+
+/**
+ * @author 	Josh Williams
+ * @date	10Jun2013
+ */
+public class CoreNlpParser {
+
+	Properties props;
+	StanfordCoreNLP pipeline;
+	ExtractionService extractor = new ExtractionService();
+	
+	/**
+	 * Default constructor
+	 * 
+	 * Creates a generic CoreNLP instance to annotate with the tokenize, split
+	 * and parse options.
+	 */
+	public CoreNlpParser() {
+		props = new Properties();
+		props.put("annotators", "tokenize, ssplit, parse");
+		pipeline = new StanfordCoreNLP(props);
+	}
+	
+	
+	/**
+	 * Creates a CoreNLP instance to annotate with the specified annotators
+	 * 
+	 * @param annotatorsString String - comma separated list of annotators to
+	 * 		perform when parsing. 
+	 */
+	public CoreNlpParser(String annotatorsString) {
+		props = new Properties();
+		props.put("annotators", annotatorsString);
+		pipeline = new StanfordCoreNLP(props);
+	}
+    
+	public List<Tree> getTextAnnotatedTree(String text) {
+		ArrayList<Tree> results = new ArrayList<Tree>();
+		List<CoreMap> annotationResults = annotate(text);
+		for(CoreMap sentence : annotationResults) {
+			results.add(sentence.get(TreeAnnotation.class));
+		}
+		return results;
+	};
+	
+	public List<SemanticGraph> getTextDependencyTree(String text) {
+		List<CoreMap> annotateResults = annotate(text);
+		ArrayList<SemanticGraph> results = new ArrayList<SemanticGraph>(); 
+		for(CoreMap sentence : annotateResults) {
+			results.add(sentence.get(CollapsedCCProcessedDependenciesAnnotation.class));
+		}
+		return results;
+	}
+	
+	public List<String> getRootChildrenAsString(String text) {
+		List<SemanticGraph> dependencies = getTextDependencyTree(text);
+		ArrayList<String> result = new ArrayList<String>();
+		
+		String resultString = "";
+		for(SemanticGraph graph : dependencies) {
+			Collection<IndexedWord> roots = graph.getRoots();
+			
+			for(IndexedWord r : roots) {
+				resultString += ("Root:\n");
+				resultString += (r.toString() + "\n");
+				resultString += ("Child Pairs:\n");
+				try {
+					resultString += (graph.childPairs(r).toString() + "\n");
+				} catch (IllegalArgumentException e) {
+					resultString += ("N/A \n");
+				}
+
+				resultString += ("Child Relations:\n");
+				try {
+					resultString += (graph.childRelns(r).toString() + "\n");
+				} catch (IllegalArgumentException e) {
+					resultString += ("N/A \n");
+				}
+			}
+			
+			result.add(resultString);
+			resultString = "";
+		}
+		
+		return result;
+	};
+	
+	public List<String> getDotNotation(String text) {
+		List<SemanticGraph> dependencies = getTextDependencyTree(text);
+		ArrayList<String> result = new ArrayList<String>();
+				
+		for(SemanticGraph graph : dependencies) {
+			result.add(graph.toDotFormat());
+		}
+		return result;
+	}
+	
+	public List<String> getEdgeVertexNotationAsString(String text) {
+		List<SemanticGraph> dependencies = getTextDependencyTree(text);
+		ArrayList<String> result = new ArrayList<String>();
+		
+		String rstring = "";		
+		for(SemanticGraph graph : dependencies) {
+			rstring += ("Edges:\n");
+			//rstring += (graph.getEdgeSet());
+			rstring += ("\nVertices:\n");
+			rstring += (graph.vertexSet());
+			
+			result.add(rstring);
+			rstring = "";
+		}
+		return result;
+	}
+	
+	/**
+	 * Perform the CoreNLP annotation on the entry string, returning the results
+	 * as a List of {@link CoreMap} objects corresponding to each sentence. </br>
+	 * </br>
+	 * <i>Basically just breaks the text into sentences.</i>
+	 *  
+	 * @param entryString String - The string to be annotated
+	 * @return List - List of {@link CoreMap} objects, each corresponding to
+	 * 		one sentence of the entryString parameter.
+	 * @see edu.stanford.nlp.util.CoreMap
+	 */
+	protected List<CoreMap> annotate(String entryString) {
+		Annotation document = new Annotation(entryString);
+	    
+	    long startMilli = new Date().getTime();
+	    pipeline.annotate(document);
+	    long diffTime = new Date().getTime() - startMilli;
+	   // log.debug("Parsing took " + diffTime + "ms");
+	    
+	    List<CoreMap> sentences = document.get(SentencesAnnotation.class);
+	    
+	    return sentences;
+	}
+	
+	/**
+	 * Return the CoreNLP annotated and parsed result of the entryString as a
+	 * String
+	 * 
+	 * @param entryString String - The string to be processed
+	 * @return String - the flattened tree returned by {@link #annotate(String) annotate}
+	 */
+	public String parseToString(String entryString) {
+		List<CoreMap> sentences = annotate(entryString);
+		
+		String ret = "";
+	     
+	    for(CoreMap sentence: sentences) {
+	    	Tree tree = sentence.get(TreeAnnotation.class);
+	    	ret = ret + tree.toString() + " ";
+	    }
+	    
+	    return ret;
+	}
+	
+	/**
+	 * Return the CoreNLP annotated and parsed result of the entryString as a
+	 * ArrayList of {@link Tree} objects
+	 * 
+	 * @param entryString String - The string to be processed
+	 * @return ArrayList - ArrayList containing {@link Tree} objects
+	 * 		representing each string
+	 */
+	public ArrayList<Tree> parse(String entryString) {
+		List<CoreMap> sentences = annotate(entryString);
+	    
+	    ArrayList<Tree> ret = new ArrayList<Tree>();
+	     
+	    for(CoreMap sentence: sentences) {
+	    	Tree tree = sentence.get(TreeAnnotation.class);
+	    	ret.add(tree);
+	    }
+	    
+	    return ret;
+	}
+
+	/**
+	* Return the CoreNLP annotated and parsed result of the entryString as a
+	* Array of {@link Tree} objects formated to remove periods.
+	 * @param entryString String - The string to be processed
+	 * @return Array - Array containing {@link Tree} objects
+	 * 		representing each string
+	**/
+	public Tree[] parseAndRemovePeriods(String entryString) {
+		List<CoreMap> sentences = annotate(entryString);
+	    
+	    ArrayList<Tree> ret = new ArrayList<Tree>();
+	     
+	    for(CoreMap sentence: sentences) {
+	    	Tree tree = sentence.get(TreeAnnotation.class);
+	    	int numKids = tree.lastChild().numChildren();
+	    	String last = tree.lastChild().lastChild().toString();
+	    	if(last.indexOf("(. ") == 0) {
+	    		tree.lastChild().removeChild(numKids -1);
+	    	}
+	    	ret.add(tree);
+	    }
+	    Tree[] arr = ret.toArray(new Tree[ret.size()]);
+	    
+	    return arr;
+	}
+
+	/**
+	 * 
+	 * @param textBlock String - The block of text to parse and extract
+	 * @return ArrayList<{@link Triplet}> - The extracted tuples
+	**/
+	public ArrayList<Triplet> parseText(String textBlock) {
+		Tree[] sentences = parseAndRemovePeriods(textBlock);
+		ArrayList<Triplet> returnArray = new ArrayList<Triplet>();
+
+		Triplet result;
+		for(Tree sentence : sentences) {
+			result = extractor.extractTriplet(sentence);
+			if(null != result) {
+				returnArray.add(result);
+			} else {
+				System.out.println("No triplet found during extraction for " + sentence);
+			}
+		}
+
+		return returnArray;
+	}
+	
+	/*
+	public void checkParserFeedback() {
+		Tree tree = parser.parse("My dog has fleas").get(0);
+		log.info(tree.label());
+		log.info(tree.children()[0].label());
+		log.info(tree.children()[0].children()[0].label());
+	}
+	*/
+		
+	public static void main(String[] args) {
+		//ExtractionService extractor = new ExtractionService();
+		
+		CoreNlpParser parser = new CoreNlpParser();
+		
+		String sentence = "The strongest rain ever recorded in India shut down the financial hub of Mumbai, snapped communication lines, closed airports and forced thousands of people to sleep in their offices or walk home during the night, officials said today.";
+		
+		Tree root = parser.parseAndRemovePeriods(sentence)[0];
+
+		List<Triplet> output = parser.parseText(sentence);
+
+		if(output.size() == 0)
+			System.err.println("ERROR: Could not find triplet");
+		else {
+			for(Triplet out : output) {
+				System.out.println( out.toString() );
+			}
+		}
+
+		String testString = "The strongest rain ever recorded in India shut down the financial hub of Mumbai, snapped communication lines, closed airports and forced thousands of people to sleep in their offices or walk home during the night, officials said today.";
+		
+		List<SemanticGraph> dependencies = parser.getTextDependencyTree(testString);
+		
+		if(dependencies.size() == 0)
+			System.err.println("ERROR: Could not find dependencies");
+		else {
+			
+			for(SemanticGraph graph : dependencies) {
+
+				System.out.println("Graph:");
+				System.out.println(graph.toString());
+
+				Collection<IndexedWord> roots = graph.getRoots();
+				
+				for(IndexedWord r : roots) {
+					System.out.println("Root:");
+					System.out.println(r.toString() );
+					System.out.println("Child Pairs:");
+					System.out.println(graph.childPairs(r).toString() );					
+				}
+				
+				System.out.println();
+				
+				System.out.println(graph.toString() );
+				System.out.println(graph.toDotFormat() );
+				System.out.println(graph.getRoots().toString());
+				System.out.println(graph.childRelns(graph.getFirstRoot()).toString() );
+				System.out.println(graph.childPairs(graph.getFirstRoot()).toString() );
+				//System.out.println(graph.getEdgeSet().toString());
+				System.out.println(graph.vertexSet().toString());
+			}
+		}
+
+	}
+}
